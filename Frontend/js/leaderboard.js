1a54a2d5de60d323795be4e51a7393bd931e9ac9
suzu <sankharsujita@gmail.com>
2025-11-13 07:25:27 +0545

    Second commit changes

--- Frontend/js/leaderboard.js
+++ Frontend/js/leaderboard.js
@@ -10,16 +10,15 @@ function loadLeaderboard() {
     .then(scores => {
       leaderboardBody.innerHTML = "";
       if (!scores.length) {
-        leaderboardBody.innerHTML = "<tr><td colspan='5'>No scores yet.</td></tr>";
+        leaderboardBody.innerHTML = "<tr><td colspan='4'>No scores yet.</td></tr>";
         return;
       }
 
-      let totalScore = 0;
       scores.forEach((entry, i) => {
-        totalScore += +entry.score;
         leaderboardBody.innerHTML += `
           <tr>
             <td>${i+1}</td>
+            <td>${entry.username}</td>
             <td>${entry.score}</td>
             <td>${new Date(entry.date_played).toLocaleString()}</td>
              <td>${entry.level}</td>
@@ -27,13 +26,7 @@ function loadLeaderboard() {
         `;
       });
 
-      // Total score row
-      leaderboardBody.innerHTML += `
-        <tr>
-          <td colspan="2"><strong>Total</strong></td>
-          <td colspan="3"><strong>${totalScore}</strong></td>
-        </tr>
-      `;
+      // Removed total score row
     })
     .catch(err => console.error(err));
 }
